9321e9409e12b1fc7566612d814b37529f1b9c99
Tatenda <31291528+rideam@users.noreply.github.com>
2023-09-26 17:38:31 +0200

    fix lock icon

--- complete-application/src/main/java/io/fusionauth/quickstart/springweb/SpringwebApplication.java
+++ complete-application/src/main/java/io/fusionauth/quickstart/springweb/SpringwebApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FusionAuthSpringApplication {
-
+public class SpringwebApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(FusionAuthSpringApplication.class, args);
+		SpringApplication.run(SpringwebApplication.class, args);
 	}
-
 }
